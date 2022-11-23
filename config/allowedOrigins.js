db7760b0ea30859760f274fee07a081d8373a983
armoako <frank.amoako-dev@outlook.com>
2022-11-23 22:17:15 +0000

    ready to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://blogapp-hx3h.onrender.com",
-]
+const allowedOrigins = ["https://blogapp-hx3h.onrender.com"]
 
 module.exports = allowedOrigins
